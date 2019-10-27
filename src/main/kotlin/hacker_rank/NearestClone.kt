1fcb82e2849ffcde9b47bc21543ecde72c22cdd8
James Cain <cain.jay@gmail.com>
2019-10-27 12:46:12 -0600

    remove DFS solution

--- src/main/kotlin/hacker_rank/NearestClone.kt
+++ src/main/kotlin/hacker_rank/NearestClone.kt
@@ -60,35 +60,6 @@ private fun findShortestPathBFS(node: GraphNode, color: Long, startId: Int): Int
     return -1
 }
 
-fun findShortestWithDFS(graphNodes: Int, graphFrom: IntArray, graphTo: IntArray, ids: LongArray, `val`: Int): Int {
-    // create nodes with ids
-    val graph = Array(graphNodes) { GraphNode(it + 1, ids[it]) }
-
-    //add edges
-    for (index in graphTo.indices) {
-        val to = graph[graphTo[index] - 1]
-        val from = graph[graphFrom[index] - 1]
-        to.addNeighbors(from)
-        from.addNeighbors(to)
-    }
-
-    val start = findStart(`val`.toLong(), graph)
-
-    val path = findShortestPathDFS(setOf(start), setOf(start), `val`, start.id)
-    return if (path.isNotEmpty()) path.minBy { it.size }!!.size - 1 else -1
-}
-
-fun findShortestPathDFS(path: Set<GraphNode>, visited: Set<GraphNode>, color: Int, startId: Int): List<Set<GraphNode>> {
-    val node = path.last()
-    if (node.color == color.toLong() && node.id != startId) {
-        return listOf(path)
-    }
-
-    return node.neighbors.filter { !visited.contains(it) }.map { neighbor ->
-        findShortestPathDFS(path + neighbor, visited + neighbor, color, startId)
-    }.flatten()
-}
-
 fun main() {
 
     val graphNodesEdges = scanner.nextLine().split(" ")
