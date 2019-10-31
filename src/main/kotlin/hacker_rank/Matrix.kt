716386ed57db7ec38f8755dc433d47fc30354de2
cainj <cain.jay@gmail.com>
2019-10-30 18:40:21 -0600

    kruskal algo

--- src/main/kotlin/hacker_rank/Matrix.kt
+++ src/main/kotlin/hacker_rank/Matrix.kt
@@ -2,41 +2,50 @@ package hacker_rank
 
 import java.util.*
 
-typealias RoadTime = Pair<City, Int>
+class RoadSortOrder : Comparator<Road> {
+    override fun compare(o1: Road?, o2: Road?): Int {
+        return o1?.time?.plus(o2?.time ?: 0) ?: 0
+    }
+}
 
-class City(val id: Int, val roads: HashSet<RoadTime> = hashSetOf(), val hasMachine: Boolean = false)
+data class Road(val to: Int, val from: Int, val time: Int)
 
 // Complete the minTime function below.
-fun minTime(roads: Array<Array<Int>>, machines: Array<Int>): Int {
+fun minTime(arr: Array<Array<Int>>, machines: Array<Int>): Int {
 
     //setup adjacency list
-    val cities = Array(roads.size + 1) { City(it, hasMachine = hasMachine(it, machines)) }
-    for (road in roads) {
-        val city1 = cities[road[0]]
-        val city2 = cities[road[1]]
-        city1.roads.add(city2 to road[2])
-        city2.roads.add(city1 to road[2])
+    val roads = Array(arr.size) {
+        Road(arr[it][0], arr[it][1], arr[it][2])
     }
 
-    //traverse through the roads and find all cities that have machines
-    //then find all possible paths between those cities and the machines
-    val start = cities.find { it.hasMachine }!!
-    val initialPath = hashSetOf(start to 0)
-    val visited = hashSetOf(start)
-    val goal = findGoal(start, machines)
-    findPaths(initialPath, visited, goal)
+    //Sort the roads
+    roads.sortWith(RoadSortOrder())
+
+    val disjointSet = DisjointSet<Int>()
+    for (city in 0..arr.size + 1)
+        disjointSet.makeSet(city)
+
+
+    val result = mutableListOf<Edge>()
+
+    for (edge in roads) {
+        if (isNotCycle(edge, disjointSet)) {
+            val root1 = disjointSet.findSet(edge.to)
+            val root2 = disjointSet.findSet(edge.from)
+
+            if (root1 == root2)
+                continue
+            else {
+                result.add(edge)
+                disjointSet.union(edge.to, edge.from)
+            }
+
+        }
+    }
     return 0
 }
 
-fun findGoal(city: City, machines: Array<Int>): Int {
-    return machines.find { it != city.id }!!
-}
-
 fun hasMachine(city: Int, machines: Array<Int>): Boolean = machines.contains(city)
-
-fun findPaths(path: Set<RoadTime>, visited: Set<City>, goal: Int): List<Set<Int>> {
-    TODO()
-}
 
 
 fun main() {
