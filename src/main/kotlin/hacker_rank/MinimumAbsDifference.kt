bc8cc6c8f813f52d8a35c6176139a62f3f059593
cainj <cain.jay@gmail.com>
2019-11-01 17:56:28 -0600

    MinimumAbsDifference

---
+++ src/main/kotlin/hacker_rank/MinimumAbsDifference.kt
@@ -0,0 +1,28 @@
+package hacker_rank
+
+import java.util.*
+import kotlin.math.abs
+import kotlin.math.min
+
+// Complete the minimumAbsoluteDifference function below.
+fun minimumAbsoluteDifference(arr: Array<Int>): Int {
+    arr.sort()
+    var min = abs(arr[0] - arr[1])
+    for (i in 0..arr.size - 2)
+        min = min(min, abs(arr[i] - arr[i + 1]))
+    
+    return min
+
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextLine().trim().toInt()
+
+    val arr = scan.nextLine().split(" ").map { it.trim().toInt() }.toTypedArray()
+
+    val result = minimumAbsoluteDifference(arr)
+
+    println(result)
+}
