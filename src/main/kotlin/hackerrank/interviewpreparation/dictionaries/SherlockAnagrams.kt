c70676c9ff4119196d78f22ee2aac0fe3192da4d
cainj <cain.jay@gmail.com>
2019-11-07 08:58:52 -0700

    sherlock anagrams

--- src/main/kotlin/hackerrank/interviewpreparation/dictionaries/SherlockAnagrams.kt
+++ src/main/kotlin/hackerrank/interviewpreparation/dictionaries/SherlockAnagrams.kt
@@ -4,11 +4,27 @@ import java.util.*
 
 // Complete the sherlockAndAnagrams function below.
 fun sherlockAndAnagrams(s: String): Int {
+    var anagrams = 0
+    val anagramKeys = hashMapOf<String, Int>()
 
-
+    //create map
+    for (i in s.indices) {
+        for (j in i + 1..s.length) {
+            val chars = s.substring(i, j).toCharArray()
+            chars.sort()
+            val key = String(chars)
+            if (anagramKeys.containsKey(key)) {
+                val oldValue = anagramKeys[key]!!
+                anagrams += oldValue
+                anagramKeys[key] = oldValue + 1
+            } else
+                anagramKeys[key] = 1
+        }
+    }
+    return anagrams
 }
 
-fun main(args: Array<String>) {
+fun main() {
     val scan = Scanner(System.`in`)
     val q = scan.nextLine().trim().toInt()
     for (qItr in 1..q) {
