756f4c54676d4d76808e90cfd3431730fc0aa9d5
cainj <cain.jay@gmail.com>
2019-11-08 18:10:36 -0700

    Buggy tests for this problem in hackerrank

--- src/main/kotlin/hackerrank/interviewpreparation/search/IceCreamParlor.kt
+++ src/main/kotlin/hackerrank/interviewpreparation/search/IceCreamParlor.kt
@@ -4,18 +4,28 @@ import java.util.*
 
 // Complete the whatFlavors function below.
 fun whatFlavors(cost: Array<Int>, money: Int): Unit {
+    //iterate thru the cost
+    //at each index check to see if the cost is more than the money if so ignore
+    //if not in the map then money - cost[i] as key and place the index as value
+    val spend = hashMapOf<Int, Int>()
+    for (i in cost.indices) {
+        if (spend[cost[i]] != null)
+            return println("${spend[cost[i]]!!} ${i + 1}")
 
+        if (cost[i] < money)
+            spend[money - cost[i]] = i + 1
+    }
 }
 
-fun main(args: Array<String>) {
+fun main() {
     val scan = Scanner(System.`in`)
 
-    val t = scan.nextLine().trim().toInt()
+    val t = scan.nextLine().trim().toLong()
 
     for (tItr in 1..t) {
         val money = scan.nextLine().trim().toInt()
         val n = scan.nextLine().trim().toInt()
-        val cost = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+        val cost = scan.nextLine().trim().split(" ").map { it.trim().toInt() }.toTypedArray()
 
         whatFlavors(cost, money)
     }
