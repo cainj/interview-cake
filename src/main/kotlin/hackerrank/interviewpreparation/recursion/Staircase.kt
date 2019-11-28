7d8597fc696559d10676a95ce5cba03717ee4021
cainj <cain.jay@gmail.com>
2019-11-28 11:18:35 -0700

    shell for recursion

---
+++ src/main/kotlin/hackerrank/interviewpreparation/recursion/Staircase.kt
@@ -0,0 +1,20 @@
+package hackerrank.interviewpreparation.recursion
+
+import java.util.*
+
+// Complete the stepPerms function below.
+fun stepPerms(n: Int): Int {
+    var result = 0
+    return result
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+    val s = scan.nextLine().trim().toInt()
+
+    for (sItr in 1..s) {
+        val n = scan.nextLine().trim().toInt()
+        val res = stepPerms(n)
+        println(res)
+    }
+}
