7698b1d65b676d8b5d0ced62436c0ccd7a576696
cainj <cain.jay@gmail.com>
2019-11-29 11:37:44 -0700

    shell for simplified chess

---
+++ src/main/kotlin/hackerrank/algorithms/recursions/SimplifiedChess.kt
@@ -0,0 +1,36 @@
+package hackerrank.algorithms.recursions
+
+import java.util.*
+
+/*
+ * Complete the simplifiedChessEngine function below.
+ */
+fun simplifiedChessEngine(whites: Array<Array<Char>>, blacks: Array<Array<Char>>, moves: Int): String {
+    return ""
+}
+
+fun main() {
+
+    val scan = Scanner(System.`in`)
+    val g = scan.nextLine().trim().toInt()
+
+    for (gItr in 1..g) {
+        val wbm = scan.nextLine().split(" ")
+        val w = wbm[0].trim().toInt()
+        val b = wbm[1].trim().toInt()
+        val m = wbm[2].trim().toInt()
+        val whites = Array<Array<Char>>(w) { Array<Char>(3) { 0.toChar() } }
+
+        for (whitesRowItr in 0 until w) {
+            whites[whitesRowItr] = scan.nextLine().split(" ").map { it[0] }.toTypedArray()
+        }
+
+        val blacks = Array(b) { Array(3) { 0.toChar() } }
+
+        for (blacksRowItr in 0 until b) {
+            blacks[blacksRowItr] = scan.nextLine().split(" ").map { it[0] }.toTypedArray()
+        }
+
+        simplifiedChessEngine(whites, blacks, m)
+    }
+}
