b12f89d5e97fb78e56b622391633ecc80f1de9d2
James Cain <cain.jay@gmail.com>
2019-11-29 23:55:46 -0700

    dp

--- src/main/kotlin/hackerrank/interviewpreparation/recursion/Staircase.kt
+++ src/main/kotlin/hackerrank/interviewpreparation/recursion/Staircase.kt
@@ -2,10 +2,37 @@ package hackerrank.interviewpreparation.recursion
 
 import java.util.*
 
+
 // Complete the stepPerms function below.
 fun stepPerms(n: Int): Int {
-    var result = 0
-    return result
+    if (n == 1)
+        return 1
+    val m = IntArray(n + 1)
+    m[0] = 1
+    return countSteps(n, m)
+}
+
+fun countSteps(n: Int, m: IntArray): Int {
+    return when {
+        n < 0 -> 0
+        m[n] != 0 -> m[n]
+        n == 1 -> 1
+        else -> {
+            m[n] = countSteps(n - 3, m) + countSteps(n - 2, m) + countSteps(n - 1, m)
+            m[n]
+        }
+    }
+}
+
+fun countStepsList(lead: List<Int>, steps: List<Int>, stepsLeft: Int): List<List<Int>> {
+    return when {
+        stepsLeft == 0 -> listOf(lead)
+        stepsLeft < 0 -> emptyList()
+        else -> {
+            val filtered = steps.filter { stepsLeft - it > -1 }
+            filtered.map { step -> countStepsList(lead + step, steps, stepsLeft - step) }.flatten()
+        }
+    }
 }
 
 fun main() {
