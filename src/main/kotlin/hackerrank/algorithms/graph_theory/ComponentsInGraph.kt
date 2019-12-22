ac3b0de1e0bca4eebd18bd30f38c4522dd20e052
James Cain <cain.jay@gmail.com>
2019-12-22 14:51:17 -0700

    solving short palindrome

--- src/main/kotlin/hackerrank/algorithms/graph_theory/ComponentsInGraph.kt
+++ src/main/kotlin/hackerrank/algorithms/graph_theory/ComponentsInGraph.kt
@@ -14,16 +14,13 @@ fun componentsInGraph(gb: Array<Array<Int>>): Array<Int> {
 
 fun main(args: Array<String>) {
     val scan = Scanner(System.`in`)
-
     val n = scan.nextLine().trim().toInt()
-
-    val gb = Array<Array<Int>>(n) { Array<Int>(2) { 0 } }
+    val gb = Array(n) { Array<Int>(2) { 0 } }
 
     for (gbRowItr in 0 until n) {
         gb[gbRowItr] = scan.nextLine().split(" ").map { it.trim().toInt() }.toTypedArray()
     }
 
     val result = componentsInGraph(gb)
-
     println(result.joinToString(" "))
 }
--- src/main/kotlin/hackerrank/interviewpreparation/search/ShortPalindromes.kt
+++ src/main/kotlin/hackerrank/interviewpreparation/search/ShortPalindromes.kt
@@ -2,12 +2,6 @@ package hackerrank.interviewpreparation.search
 
 import java.util.*
 
-// Complete the shortPalindrome function below.
-fun shortPalindrome(s: String): Int {
-    var solution = 0
-    return solution
-}
-
 fun main() {
     val scan = Scanner(System.`in`)
     val s = scan.nextLine()
@@ -15,3 +9,56 @@ fun main() {
 
     println(result)
 }
+
+const val MOD = 1000000007
+const val SIZE = 26
+
+// Complete the shortPalindrome function below.
+fun shortPalindrome(s: String): Long {
+    val single = Array(SIZE) { 0L }
+    val double = Array(SIZE) { Array(SIZE) { 0L } }
+    val triple = Array(SIZE) { Array(SIZE) { Array(SIZE) { 0L } } }
+    val quadruple = Array(SIZE) { Array(SIZE) { Array(SIZE) { Array(SIZE) { 0L } } } }
+
+    for (i in s) {
+        val c = i - 'a'
+        for (j in 0 until 26) {
+            quadruple[c][j][j][c] += triple[c][j][j] % MOD
+            triple[j][c][c] += double[j][c] % MOD
+            double[j][c] += single[j] % MOD
+        }
+        single[c]++
+    }
+
+    return Arrays.stream(quadruple)
+        .flatMap(Arrays::stream)
+        .flatMap(Arrays::stream)
+        .flatMapToLong { Arrays.stream(it.toLongArray()) }.sum() % MOD
+}
+
+// Complete the shortPalindrome function below.
+fun shortPalindromeBrutForce(s: String): Int {
+    var solution = 0
+
+    for (i in 0..s.length - 3) {
+        for (j in i + 3 until s.length)
+            if (s[i] == s[j])
+                solution += findMiniPalindromes(s.substring(i + 1, j))
+
+
+    }
+
+    return solution
+}
+
+
+fun findMiniPalindromes(substring: String): Int {
+    var solution = 0
+    for (m in 0 until substring.length - 1)
+        for (n in m + 1 until substring.length)
+            if (substring[m] == substring[n])
+                solution++
+
+    return solution
+}
+
