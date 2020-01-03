934dce63b7f6653c6db344d092c6ae984b755046
cainj <cain.jay@gmail.com>
2020-01-03 15:06:52 -0700

    solving max array subset.

--- src/main/kotlin/hackerrank/datastructures/arrays/MaxArraySum.kt
+++ src/main/kotlin/hackerrank/datastructures/arrays/MaxArraySum.kt
@@ -5,24 +5,19 @@ import java.util.*
 
 // Complete the maxSubsetSum function below.
 fun maxSubsetSum(arr: Array<Int>): Int {
-    var maxValue = Integer.MIN_VALUE
 
     if (arr.size < 2)
         return arr[0]
 
-    var twoBack = arr[0]
-    var oneBack = arr[1]
-    maxValue = max(twoBack, oneBack)
+    var nonAdjacentMax = arr[0]
+    var prevMax = max(nonAdjacentMax, arr[1])
+    var maxValue = Int.MIN_VALUE
 
     for (i in 2 until arr.size) {
-        val currentValue = arr[i] + twoBack
-        maxValue = if (oneBack > currentValue)
-            oneBack
-        else
-            currentValue
-
-        twoBack = oneBack
-        oneBack = maxValue
+        val currentMax = max(arr[i], prevMax)
+        maxValue = max(currentMax, arr[i] + nonAdjacentMax)
+        nonAdjacentMax = prevMax
+        prevMax = maxValue
     }
 
     return maxValue
@@ -32,7 +27,7 @@ fun main() {
     val scan = Scanner(System.`in`)
     readLine()
     val arr = scan.nextLine().split(" ").map { it.trim().toInt() }.toTypedArray()
-
     val res = maxSubsetSum(arr)
+
     println(res)
 }
