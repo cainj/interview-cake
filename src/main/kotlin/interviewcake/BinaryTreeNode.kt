89935b0ce81d0642b2240664986733efd4ae43ab
cainj <cain.jay@gmail.com>
2020-02-06 22:18:41 -0700

    cleanup

--- src/main/kotlin/interviewcake/BinaryTreeNode.kt
+++ src/main/kotlin/interviewcake/BinaryTreeNode.kt
@@ -1,7 +1,5 @@
 package interviewcake
 
-data class NodeDepthPair(val node: BinaryTreeNode, val depth: Int)
-
 class BinaryTreeNode(val value: Int, var left: BinaryTreeNode? = null, var right: BinaryTreeNode? = null) {
 
     fun insertLeft(leftValue: Int): BinaryTreeNode {
--- src/main/kotlin/interviewcake/SuperBalancedTree.kt
+++ src/main/kotlin/interviewcake/SuperBalancedTree.kt
@@ -4,11 +4,9 @@ import java.util.*
 import kotlin.math.abs
 import kotlin.math.max
 
-// determine if the tree is superbalanced
+data class NodeDepthPair(val node: BinaryTreeNode, val depth: Int)
+
 fun isBalanced(treeRoot: BinaryTreeNode): Boolean {
-    // get the height of the shortest path
-    // get the height of the longest path
-    // then take the difference of left height vs right height
     return depthNodePair(treeRoot)
 }
 
@@ -46,44 +44,6 @@ private fun depthNodePair(treeRoot: BinaryTreeNode?): Boolean {
 
     return true
 
-}
-
-fun longestPath(node: BinaryTreeNode?, height: Int): Int {
-    return when {
-        null == node -> height
-        isLeafNode(node) -> height
-        else -> max(longestPath(node.right, height + 1), longestPath(node.left, height + 1))
-    }
-}
-
-fun shortestPath(treeRoot: BinaryTreeNode?): Int {
-
-    if (treeRoot == null) return 0
-
-    val queue: ArrayDeque<List<BinaryTreeNode>> = ArrayDeque()
-    var height = 0
-    var children: List<BinaryTreeNode>
-
-    queue.push(mutableListOf(treeRoot))
-
-    while (queue.isNotEmpty()) {
-        children = queue.pop()
-        val newChildren = mutableListOf<BinaryTreeNode>()
-        for (child in children) {
-
-            if (isLeafNode(child))
-                return height
-
-            if (child.left != null)
-                newChildren.add(child.left!!)
-
-            if (child.right != null)
-                newChildren.add(child.right!!)
-        }
-        queue.push(newChildren)
-        height++
-    }
-    return height
 }
 
 fun isLeafNode(node: BinaryTreeNode): Boolean = (node.right == null && node.left == null)
