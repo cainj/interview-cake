873e5a3f4de34daebde3d12754c96c49bd68caf0
James Cain <cain.jay@gmail.com>
2020-04-11 20:21:03 -0600

    Challenge Day 11

---
+++ src/main/kotlin/leetcode/challenge/DiameterOfBinaryTree.kt
@@ -0,0 +1,24 @@
+package leetcode.challenge
+
+import leetcode.binarytrees.TreeNode
+import kotlin.math.max
+
+class DiameterOfBinaryTree {
+    var max = 1
+
+    fun diameterOfBinaryTree(root: TreeNode?): Int {
+        height(root)
+        return max - 1
+    }
+
+    private fun height(node: TreeNode?): Int =
+        if (node == null)
+            0
+        else {
+            val l = height(node.left)
+            val r = height(node.right)
+            max = max(l + r + 1, max)
+            max(l, r) + 1
+        }
+
+}
