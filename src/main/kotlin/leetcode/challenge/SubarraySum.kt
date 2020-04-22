3b812717b0f8052cfed0300362f0135bb072c00b
James Cain <cain.jay@gmail.com>
2020-04-22 12:46:36 -0600

    was able to solve with brute force and using hashMap.  Had hints for the hashMap.

---
+++ src/main/kotlin/leetcode/challenge/SubarraySum.kt
@@ -0,0 +1,35 @@
+package leetcode.challenge
+
+class SubarraySum {
+
+    fun subarraySum(nums: IntArray, k: Int): Int {
+        val cumulativeSum = hashMapOf<Int, Int>()
+        var sum = 0
+        cumulativeSum[0] = 1
+        var count = 0
+
+        for (n in nums) {
+            sum += n
+            if (cumulativeSum.contains(sum - k))
+                count += cumulativeSum[sum - k]!!
+            cumulativeSum[sum] = cumulativeSum.getOrDefault(sum, 0) + 1
+        }
+
+        return count
+    }
+      // Runtime O(n^2)
+//    fun subarraySum(nums: IntArray, k: Int): Int {
+//        var count = 0
+//        var sum = nums[0]
+//
+//        for (i in nums.indices) {
+//            sum = nums[i]
+//            for (j in i until nums.size) {
+//                if (i != j) sum += nums[j]
+//                if (sum == k) count++
+//            }
+//        }
+//
+//        return count
+//    }
+}
---
+++ src/test/kotlin/leetcode/challenge/SubarraySumTest.kt
@@ -0,0 +1,16 @@
+package leetcode.challenge
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class SubarraySumTest {
+
+    @Test
+    fun subarraySum() {
+        val ss = SubarraySum()
+        var nums = intArrayOf(1, 1, 1)
+        assertEquals(2, ss.subarraySum(nums, 2))
+        nums = intArrayOf(3, 4, 7, 2, -3, 1, 4, 2)
+        assertEquals(3, ss.subarraySum(nums, 14))
+    }
+}
