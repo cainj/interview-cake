07fab0c86201a056560b53cfc6e5f7c10238fbb9
James Cain <cain.jay@gmail.com>
2020-04-25 12:21:38 -0600

    Day 25 of the Challenge, almost there!;  reformatting.

---
+++ src/main/kotlin/leetcode/challenge/CanJump.kt
@@ -0,0 +1,33 @@
+package leetcode.challenge
+
+import kotlin.math.max
+
+class CanJump {
+
+    fun canJump(nums: IntArray): Boolean {
+        //[2,3,1,0,0,2,0,4]
+        //i = 0
+        //window = [2, 3, 1]
+        // i = 1, maxJump = 2
+        //window = [2, 3, 1, 0, 0]
+        // i = 2, maxJump = 4
+        //window = [2, 3, 1, 0,0]
+        // i = 3, maxJump = 4
+        // window no change
+        // i == maxJump / break
+
+        val goal = nums.size - 1
+        var maxJump = 0
+
+        for (i in nums.indices) {
+            maxJump = max(maxJump, nums[i] + i)
+            if (maxJump >= goal)
+                return true
+
+            if (maxJump == i)
+                break
+        }
+
+        return false
+    }
+}
