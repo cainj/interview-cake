a206827ea4bfbbb50ec11aafbddbe9f5515951e6
James Cain <cain.jay@gmail.com>
2020-05-15 19:34:06 -0600

    Iterative search on BST

--- src/main/kotlin/leetcode/recursion/SearchBst.kt
+++ src/main/kotlin/leetcode/recursion/SearchBst.kt
@@ -4,13 +4,23 @@ import leetcode.binarytrees.TreeNode
 
 class SearchBst {
 
-    fun searchBST(node: TreeNode?, `val`: Int): TreeNode? {
-        return when {
-            node == null -> return node
-            node.`val` == `val` -> return node
+    fun searchBST(node: TreeNode?, `val`: Int): TreeNode? =
+        when {
+            node == null -> node
+            node.`val` == `val` -> node
             `val` > node.`val` -> searchBST(node.right, `val`)
             else -> searchBST(node.left, `val`)
         }
 
+    fun searchBSTIter(root: TreeNode?, `val`: Int): TreeNode? {
+        var node = root
+        while (node != null) {
+            if (node.`val` == `val`) break
+            else if (node.`val` > `val`)
+                node = node.left
+            else
+                node = node.right
+        }
+        return node
     }
 }
--- src/test/kotlin/leetcode/recursion/SearchBstTest.kt
+++ src/test/kotlin/leetcode/recursion/SearchBstTest.kt
@@ -15,4 +15,14 @@ internal class SearchBstTest {
         assertEquals(TreeNode(7, TreeNode(6), TreeNode(9)), searchBst.searchBST(tree, 7))
         assertEquals(null, searchBst.searchBST(null, 7))
     }
+
+    @Test
+    fun searchBSTIter() {
+        val searchBst = SearchBst()
+        val tree = TreeNode(4, TreeNode(2, TreeNode(1), TreeNode(3)), TreeNode(7, TreeNode(6), TreeNode(9)))
+        assertEquals(tree, searchBst.searchBSTIter(tree, 4))
+        assertEquals(null, searchBst.searchBSTIter(tree, 10))
+        assertEquals(TreeNode(7, TreeNode(6), TreeNode(9)), searchBst.searchBSTIter(tree, 7))
+        assertEquals(null, searchBst.searchBSTIter(null, 7))
+    }
 }
