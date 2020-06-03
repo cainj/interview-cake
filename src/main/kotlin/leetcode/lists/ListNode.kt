63ddfdd3bb1572b9fa82c2ec2c08be4e814a6ad4
James Cain <cain.jay@gmail.com>
2020-06-02 21:13:55 -0600

    ListNode of nodes

---
+++ src/main/kotlin/leetcode/lists/ListNode.kt
@@ -0,0 +1,3 @@
+package leetcode.lists
+
+data class ListNode(val `val`: Int, var next: ListNode? = null)
--- src/main/kotlin/leetcode/lists/MyLinkedList.kt
+++ src/main/kotlin/leetcode/lists/MyLinkedList.kt
@@ -1,19 +1,16 @@
 package leetcode.lists
 
 class MyLinkedList {
-
-    /** Initialize your data structure here. */
-    data class Node(val `val`: Int, var next: Node? = null)
-
+    
     private var size: Int = 0
-    private var head: Node = Node(-1)
+    private var head: ListNode = ListNode(-1)
 
 
     /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
     fun get(index: Int): Int {
         if (index < 0 || index >= size) return -1
 
-        var current: Node? = head
+        var current: ListNode? = head
         for (i in 0..index)
             current = current?.next
         return current?.`val` ?: -1
@@ -22,7 +19,7 @@ class MyLinkedList {
     /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
     fun addAtHead(`val`: Int) {
         val temp = head.next
-        head.next = Node(`val`, temp)
+        head.next = ListNode(`val`, temp)
         size++
     }
 
@@ -34,11 +31,11 @@ class MyLinkedList {
     /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
     fun addAtIndex(index: Int, `val`: Int) {
         if (index > -1) {
-            var current: Node? = head
+            var current: ListNode? = head
             for (i in 0 until index)
                 current = current?.next
 
-            val node = Node(`val`)
+            val node = ListNode(`val`)
             node.next = current?.next
             current?.next = node
             size++
@@ -48,7 +45,7 @@ class MyLinkedList {
     /** Delete the index-th node in the linked list, if the index is valid. */
     fun deleteAtIndex(index: Int) {
         if (index > -1 && index < size) {
-            var node: Node? = head
+            var node: ListNode? = head
             for (i in 0 until index)
                 node = node?.next
 
