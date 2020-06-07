156840cd0da7bf7001c1fb762e1e59032aac0ba8
James Cain <cain.jay@gmail.com>
2020-06-07 14:56:42 -0600

    Adding documentation for interview

--- src/main/kotlin/leetcode/binarytrees/BstKthSmallest.kt
+++ src/main/kotlin/leetcode/binarytrees/BstKthSmallest.kt
@@ -1,5 +1,37 @@
 package leetcode.binarytrees
 
+/**
+ * Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+ *
+ *
+ *
+ * Example 1:
+ * Input: root = [3,1,4,null,2], k = 1
+ *     3
+ *    / \
+ *   1   4
+ *        \
+ *         2
+ *
+ * Output: 1
+ * Example 2:
+ *
+ * Input: root = [5,3,6,2,4,null,null,1], k = 3
+ *        5
+ *       / \
+ *      3   6
+ *     / \
+ *    2   4
+ *   /
+ *  1
+ *
+ *  Output: 3
+ *
+ *  Constraints:
+ *
+ * The number of elements of the BST is between 1 to 10^4.
+ * You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+ */
 class BstKthSmallest {
 
     fun kthSmallest(root: TreeNode?, k: Int): Int {
