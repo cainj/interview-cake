8990b1263d0c5cb3116cfc8976a297311efb027b
James Cain <cain.jay@gmail.com>
2020-06-16 17:45:56 -0600

    June Challenge

---
+++ src/main/kotlin/leetcode/ValidIp.kt
@@ -0,0 +1,32 @@
+package leetcode
+
+class ValidIp {
+
+    fun validIPAddress(IP: String): String =
+        when {
+            isIPv4(IP) -> "IPv4"
+            isIPv6(IP) -> "IPv6"
+            else -> "Neither"
+        }
+
+
+    private fun isIPv4(ip: String): Boolean {
+        val regex =
+            Regex("^((0|1\\d?\\d?|2[0-4]?\\d?|25[0-5]?|[3-9]\\d?)\\.){3}(0|1\\d?\\d?|2[0-4]?\\d?|25[0-5]?|[3-9]\\d?)$")
+        if (!regex.matches(ip)) return false
+
+        return true
+    }
+
+    private fun isIPv6(ip: String): Boolean {
+        val s = ip.split(':')
+        val regex = Regex("[0-9a-fA-F]{1,4}")
+
+        if (s.size != 8) return false
+
+        for (num in s)
+            if (!regex.matches(num)) return false
+
+        return true
+    }
+}
---
+++ src/test/kotlin/leetcode/ValidIpTest.kt
@@ -0,0 +1,18 @@
+package leetcode
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class ValidIpTest {
+
+    @Test
+    fun validIPAddress() {
+        val v = ValidIp()
+        assertEquals("Neither", v.validIPAddress("256.256.256.256"))
+        assertEquals("IPv4", v.validIPAddress("255.255.255.255"))
+        assertEquals("IPv6", v.validIPAddress("2001:0db8:85a3:0:0:8A2E:0370:7334"))
+        assertEquals("Neither", v.validIPAddress(""))
+        assertEquals("Neither", v.validIPAddress("02001:0db8:85a3:0:0:8A2E:0370:7334"))
+    }
+}
