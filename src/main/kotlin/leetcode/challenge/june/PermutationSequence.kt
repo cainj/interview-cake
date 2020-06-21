5b0f9111749f9182c183db082b467cba6c93dc18
James Cain <cain.jay@gmail.com>
2020-06-21 01:12:34 -0600

    June Challenge

--- src/main/kotlin/leetcode/challenge/june/PermutationSequence.kt
+++ src/main/kotlin/leetcode/challenge/june/PermutationSequence.kt
@@ -1,4 +1,37 @@
 package leetcode.challenge.june
 
 class PermutationSequence {
+    private var count = 0
+    fun getPermutation(n: Int, k: Int): String {
+        count = 0
+        return backtrack(StringBuilder(), n, k, BooleanArray(n + 1), mutableListOf())[0]
+    }
+
+    private fun backtrack(
+        sb: StringBuilder,
+        n: Int,
+        k: Int,
+        seen: BooleanArray,
+        ans: MutableList<String>
+    ): List<String> {
+        if (sb.length == n) {
+            count++
+            return if (count == k) {
+                ans.add(sb.toString())
+                ans
+            } else ans
+        }
+
+        val size = sb.length
+        for (i in 1..n)
+            if (!seen[i]) {
+                seen[i] = true
+                sb.append(i)
+                backtrack(sb, n, k, seen, ans)
+                sb.setLength(size)
+                seen[i] = false
+            }
+
+        return ans
+    }
 }
--- src/test/kotlin/leetcode/challenge/june/PermutationSequenceTest.kt
+++ src/test/kotlin/leetcode/challenge/june/PermutationSequenceTest.kt
@@ -1,10 +1,15 @@
 package leetcode.challenge.june
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 
 internal class PermutationSequenceTest {
 
     @org.junit.jupiter.api.Test
     fun getPermutation() {
+        val p = PermutationSequence()
+        assertEquals("213", p.getPermutation(3, 3))
+        assertEquals("2314", p.getPermutation(4, 9))
+        assertEquals("231", p.getPermutation(3, 4))
+        assertEquals("321", p.getPermutation(3, 6))
     }
 }
