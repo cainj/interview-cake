048c19729dc0c33b7a9a322fd6d63cf8c6adec52
James Cain <jamescain@Jamess-MBP-2.domain>
2020-06-20 18:57:51 -0600

    June Challenge

---
+++ src/test/kotlin/leetcode/challenge/june/PermutationSequenceTest.kt
@@ -0,0 +1,10 @@
+package leetcode.challenge.june
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class PermutationSequenceTest {
+
+    @org.junit.jupiter.api.Test
+    fun getPermutation() {
+    }
+}
