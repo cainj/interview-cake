e591364a95ba2d0381b8e886262526f5c128bc5d
James Cain <cain.jay@gmail.com>
2020-06-27 08:47:12 -0600

    documentation for an interview.

--- src/main/kotlin/leetcode/challenge/june/PhoneNumber.kt
+++ src/main/kotlin/leetcode/challenge/june/PhoneNumber.kt
@@ -1,5 +1,35 @@
 package leetcode.challenge.june
 
+/**
+ * Given a string containing digits from 2-9 inclusive, return all possible
+ * letter combinations that the number could represent.
+ * A mapping of digit to letters (just like on the telephone buttons) is given below.
+ *
+ * Note that 1 does not map to any letters.
+ *
+ * '2' to "abc",
+ * '3' to "def",
+ * '4' to "ghi",
+ * '5' to "jkl",
+ * '6' to "mno",
+ * '7' to "pqrs",
+ * '8' to "tuv",
+ * '9' to "wxyz"
+ *
+ * Example: "2"
+ *
+ * Output: ["a", "b", "c"]
+ *
+ * Example:
+ * Input: "23"
+ *
+ * Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ * Note:
+ *
+ * Although the above answer is in lexicographical order, your answer could be in any order
+ * you want.
+ *
+ */
 class PhoneNumber {
 
     private val numberPad = hashMapOf<Char, String>(
