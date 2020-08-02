f92eb0a8e8aa62a9244a59027b943344a5d405c7
James Cain <cain.jay@gmail.com>
2020-08-02 10:10:55 -0600

    Formatting

--- src/main/kotlin/leetcode/challenge/august/LogRateLimiter.kt
+++ src/main/kotlin/leetcode/challenge/august/LogRateLimiter.kt
@@ -5,24 +5,22 @@ import kotlin.math.abs
 class LogRateLimiter {
 
     /** Initialize your data structure here. */
-
     private val hash = hashMapOf<String, Int>()
 
     /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
     If this method returns false, the message will not be printed.
     The timestamp is in seconds granularity. */
     fun shouldPrintMessage(timestamp: Int, message: String): Boolean =
-        when(hash[message]) {
+        when (hash[message]) {
             null -> {
                 hash[message] = timestamp
                 true
             }
-            else ->{
-                if(abs(timestamp - hash[message]!!) > 9){
+            else -> {
+                if (abs(timestamp - hash[message]!!) > 9) {
                     hash[message] = timestamp
                     true
-                }
-                else false
+                } else false
             }
         }
 }
