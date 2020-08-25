ee11b44be72418eca619113a3221cc4bcf78d091
James Cain <cain.jay@gmail.com>
2020-08-24 22:31:10 -0600

    recursive

--- src/main/kotlin/leetcode/challenge/august/SumLeftLeaves.kt
+++ src/main/kotlin/leetcode/challenge/august/SumLeftLeaves.kt
@@ -5,22 +5,33 @@ import java.util.*
 
 class SumLeftLeaves {
 
-    fun sumOfLeftLeaves(root: TreeNode?,isLeft:Boolean = false): Int {
-        if(root == null) return 0
-        if (root.left == null && root.right == null){
-            return if(isLeft) root.`val` else 0
+    fun sumOfLeftLeaves(root: TreeNode?): Int {
+        val root = root ?: return 0
+        var sum = 0
+
+        val deque = ArrayDeque<TreeNode>()
+        deque.offer(root)
+
+        while (deque.isNotEmpty()) {
+            val node = deque.poll()
+
+            if (node.left != null) {
+                if (node.left?.left == null && node.left?.right == null) sum += node.left!!.`val`
+                deque.offer(node.left!!)
+            }
+
+            if (node.right != null) deque.offer(node.right!!)
         }
-        return sumOfLeftLeaves(root.left,true) + sumOfLeftLeaves(root.right,false)
+
+        return sum
     }
 
     private var sum = 0
-    fun sumOfLeftLeavesDFS(root: TreeNode?): Int {
-        if (root == null) return 0
-
-        if (root.left != null && root.left?.left == null && root.left?.right == null) sum += root.left!!.`val`
-
-        sumOfLeftLeaves(root.left)
-        sumOfLeftLeaves(root.right)
-        return sum
+    fun sumOfLeftLeavesRecursive(root: TreeNode?,isLeft:Boolean = false): Int {
+        if(root == null) return 0
+        if (root!!.left == null && root.right == null){
+            return if(isLeft) root.`val` else 0
+        }
+        return sumOfLeftLeaves(root.left,true) + sumOfLeftLeaves(root.right,false)
     }
 }
